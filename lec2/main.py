4ddf082f7ee0da2f29ffb6addd981e2c47b576df
AlexM-dev <dogon.dogon@mail.ru>
2022-06-02 14:04:30 +0300

    anim add

--- lec2/main.py
+++ lec2/main.py
@@ -1,4 +1,4 @@
-import csv
+'''import csv
 from matplotlib.axes import Axes
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,10 +34,75 @@ pylab.legend()
 
 pylab.subplot (1, 2, 2)
 x = np.linspace(0, 100, 1000000)
-y = abs(0.25 * x + (np.cos(x * 100)) * np.sin(0.25 * x))
-pylab.plot(x, y, linewidth=2.0, label='numPy func')
+i = 0.0
 pylab.title('Func')
 pylab.xlabel('X')
 pylab.ylabel('Y') 
 pylab.legend()
+while True:
+    y = abs(0.25 * x + (np.cos(x * 100)) * np.sin(0.25 * x)) + i
+    pylab.plot(x, y, linewidth=2.0, label='numPy func')
+    i+=0.1
+    plt.show()'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import csv
+from math import sin, cos
+from matplotlib.animation import FuncAnimation
+plt.style.use('dark_background')
+
+x1 = []
+y1 = []
+
+x2 = []
+y2 = []
+with open('file.csv') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        if row['category'] == 'Offshore minerals' and row['variable'] == 'GDP':
+            y1.append(int(row['data_value']))
+            x1.append(int(row['year']))
+        elif row['category'] == 'Shipping' and row['variable'] == 'GDP':
+            y2.append(int(row['data_value']))
+            x2.append(int(row['year']))
+
+x = np.linspace(0,15,100)
+
+fig = plt.figure()
+p1 = fig.add_subplot(121)
+p2 = fig.add_subplot(122)
+
+'''p1.title('GDP')
+p1.xlabel('Year')
+p1.ylabel('Value') 
+p1.legend()
+
+p2.title('Sin')
+p2.xlabel('X')
+p2.ylabel('Y') 
+p2.legend()'''
+
+p2.set_xlim([0, 4])
+p2.set_ylim([-1.5, 1.5])
+
+# set up empty lines to be updates later on
+l1, = p2.plot([],[],'b')
+
+l2, = p1.plot(x1, y1, label='Offshore minerals')
+l3, = p1.plot(x2, y2, label='Shipping')
+
+
+def init():
+    l1.set_data([], [])
+    return l1,
+def animate(i):
+    x = np.linspace(0, 4, 1000)
+    y = np.sin(np.pi * (x - 0.01 * i))
+    l1.set_data(x, y)
+    return l1,
+
+anim = FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
 plt.show()
